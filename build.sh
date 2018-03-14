2d9ca3d5e632806447a9910db8a28f962d81bde0
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-03-14 14:36:49 +0000

    Updated commit

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #     echo "Auto Update UI Mode"
 #     GIT_REF="master"
 # fi
-GIT_REF="1becbc1acf2b3feed8ab635df6c3f1ce5e2b26d0"
+GIT_REF="b95d159f639988f26373d47e5a35aeed415400af"
 
 
 
